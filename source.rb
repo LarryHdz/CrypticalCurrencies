7f3741e6f31ac3e00e015281c3a5f281eb6377fc
Lawrence <lawrence.hernandez01@utrgv.edu>
2018-04-22 14:26:48 -0500

    1-update

---
+++ source.rb
@@ -0,0 +1,87 @@
+require 'sinatra'
+require 'data_mapper' # metagem, requires common plugins too.
+require 'net/http'
+require 'json'
+require 'openssl'
+
+if ENV['DATABASE_URL']
+  DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+else
+  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+end
+
+
+
+
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+def get_search_count(term)
+
+uri = URI("https://www.udemy.com/api-2.0/search-suggestions?q=#{term}")
+search_amount = Net::HTTP.get(uri)
+web_hash = JSON.parse(search_amount)
+hash_value = web_hash ["results"][0]["searched_count"]
+return hash_value
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Coin
+    include DataMapper::Resource
+    property :id, Serial
+    property :title, String
+    property :body, Text
+    property :created_at, DateTime
+end
+
+# Perform basic sanity checks and initialize all relationships
+# Call this when you've defined all your models
+DataMapper.finalize
+
+# automatically create the post table
+Coin.auto_upgrade!
+
+get '/' do
+	#erb :stocks
+	#return "this is the main page where the stocks will be shown here"
+	erb :info
+end
+
+get '/about_us' do
+	#erb :info
+	#return "info about us here"
+end
+
+get '/new_account' do
+	#erb :new
+	return "create a new account here"
+end
+
+get '/login' do
+	#erb :login
+	return "login here"
+end
+
+post '/my_account' do 
+	#erb:account
+	return "your account is here"
+end
+
+
+
